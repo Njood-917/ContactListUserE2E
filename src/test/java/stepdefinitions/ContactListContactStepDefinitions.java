acc21fd2a66b8b88940a1601174f8c9101021803
Njood-917 <njood9177@gmail.com>
2024-05-04 20:33:44 +0300

    task 16 done

--- src/test/java/stepdefinitions/ContactListContactStepDefinitions.java
+++ src/test/java/stepdefinitions/ContactListContactStepDefinitions.java
@@ -2,11 +2,15 @@ package stepdefinitions;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.javafaker.Faker;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.response.Response;
 import pojos.ContactPojo;
+import pojos.UserPojo;
+import pojos.UserResponsePojo;
 
 import static base_urls.ContactListBaseUrl.spec;
 import static io.restassured.RestAssured.given;
@@ -19,14 +23,14 @@ public class ContactListContactStepDefinitions {
     @Given("set the url for adding contact")
     public void set_the_url_for_adding_contact() {
         //https://thinking-tester-contact-list.herokuapp.com/contacts
-        spec.pathParams("first","contacts");
+        spec.pathParams("first", "contacts");
     }
 
     @Given("set the expected data for adding contact")
     public void set_the_expected_data_for_adding_contact() throws JsonProcessingException {
         String strJson = """
                 {
-                    "firstName": "John",
+                    "firstName": "Lama",
                     "lastName": "Doe",
                     "birthdate": "1970-01-01",
                     "email": "jdoe@fake.com",
@@ -47,8 +51,8 @@ public class ContactListContactStepDefinitions {
 
     @When("send the post request for adding contact")
     public void send_the_post_request_for_adding_contact() {
-          response = given(spec).body(expectedData).post("{first}");
-          response.prettyPrint();
+        response = given(spec).body(expectedData).post("{first}");
+        response.prettyPrint();
     }
 
     @Then("do assertion for adding contact")
@@ -58,10 +62,167 @@ public class ContactListContactStepDefinitions {
 
         assertEquals(201, response.statusCode());
         assertEquals(expectedData.getFirstName(), actualData.getFirstName());
-        //Rest is homework!!
+        assertEquals(expectedData.getLastName(), actualData.getLastName());
+        assertEquals(expectedData.getBirthdate(), actualData.getBirthdate());
+        assertEquals(expectedData.getCity(), actualData.getCity());
+        assertEquals(expectedData.getCountry(), actualData.getCountry());
+        assertEquals(expectedData.getPhone(), actualData.getPhone());
+        assertEquals(expectedData.getEmail(), actualData.getEmail());
+        assertEquals(expectedData.getStreet1(), actualData.getStreet1());
+        assertEquals(expectedData.getStreet2(), actualData.getStreet2());
+        assertEquals(expectedData.getPostalCode(), actualData.getPostalCode());
+        assertEquals(expectedData.getStateProvince(), actualData.getStateProvince());
 
 
     }
 
 
+// update contact
+
+    @Given("set the url for update contact")
+    public void setTheUrlForUpdateContact() {
+        spec.pathParams("first", "contacts","second","66366c0c2e101a0013feadb0");
+    }
+
+    @And("set the expected data for update contact")
+    public void setTheExpectedDataForUpdateContact() throws JsonProcessingException {
+        String strJson = """
+                {
+                    "firstName": "Lama2",
+                    "lastName": "Doe",
+                    "birthdate": "1970-01-01",
+                    "email": "jdoe@fake.com",
+                    "phone": "8005555555",
+                    "street1": "1 Main St.",
+                    "street2": "Apartment A",
+                    "city": "Anytown",
+                    "stateProvince": "KS",
+                    "postalCode": "12345",
+                    "country": "USA"
+                }""";
+
+        expectedData = new ObjectMapper().readValue(strJson, ContactPojo.class);
+        System.out.println("expectedData = " + expectedData);
+
+
+//        expectedData = new ContactPojo("Amy",
+//                "Miller",
+//               "Canada",
+//                "1992-02-02",
+//               "8005554242",
+//                "Washington",
+//                "A1A1A1",
+//                "QC",
+//                "13 School St.",
+//                "Apt. 5",
+//                "amiller@fake.com");
+//        System.out.println("expectedData = " + expectedData);
+
+    }
+
+    @When("send the put request for update contact")
+    public void sendThePutRequestForUpdateContact() {
+        response = given(spec).body(expectedData).put("{first}/{second}");
+        response.prettyPrint();
+    }
+
+    @Then("do assertion for update contact")
+    public void doAssertionForUpdateContact() {
+        ContactPojo actualData = response.as(ContactPojo.class);
+        System.out.println("actualData = " + actualData);
+        assertEquals(200, response.statusCode());
+        assertEquals(expectedData.getFirstName(), actualData.getFirstName());
+        assertEquals(expectedData.getLastName(), actualData.getLastName());
+        assertEquals(expectedData.getBirthdate(), actualData.getBirthdate());
+        assertEquals(expectedData.getCity(), actualData.getCity());
+        assertEquals(expectedData.getCountry(), actualData.getCountry());
+        assertEquals(expectedData.getPhone(), actualData.getPhone());
+        assertEquals(expectedData.getEmail(), actualData.getEmail());
+        assertEquals(expectedData.getStreet1(), actualData.getStreet1());
+        assertEquals(expectedData.getStreet2(), actualData.getStreet2());
+        assertEquals(expectedData.getPostalCode(), actualData.getPostalCode());
+        assertEquals(expectedData.getStateProvince(), actualData.getStateProvince());
+    }
+
+    //read contact
+
+    @Given("set the url for Read")
+    public void setTheUrlForRead() {
+        spec.pathParams("first", "contacts","second","6636706b0a93760013f0d197");
+
+
+    }
+
+    @And("set the expected data for read")
+    public void setTheExpectedDataForRead() throws JsonProcessingException {
+        String strJson = """
+                {
+                    "firstName": "Lama2",
+                    "lastName": "Doe",
+                    "birthdate": "1970-01-01",
+                    "email": "jdoe@fake.com",
+                    "phone": "8005555555",
+                    "street1": "1 Main St.",
+                    "street2": "Apartment A",
+                    "city": "Anytown",
+                    "stateProvince": "KS",
+                    "postalCode": "12345",
+                    "country": "USA"
+                }""";
+
+        expectedData = new ObjectMapper().readValue(strJson, ContactPojo.class);
+        System.out.println("expectedData = " + expectedData);
+
+
+    }
+
+    @When("send the get request for read")
+    public void sendTheGetRequestForRead() {
+        response = given(spec).get("{first}/{second}");
+        response.prettyPrint();
+
+    }
+
+    @Then("do assertion for read")
+    public void doAssertionForRead() {
+        ContactPojo actualData = response.as(ContactPojo.class);
+        System.out.println("actualData = " + actualData);
+        assertEquals(200, response.statusCode());
+        assertEquals(expectedData.getFirstName(), actualData.getFirstName());
+        assertEquals(expectedData.getLastName(), actualData.getLastName());
+        assertEquals(expectedData.getBirthdate(), actualData.getBirthdate());
+        assertEquals(expectedData.getCity(), actualData.getCity());
+        assertEquals(expectedData.getCountry(), actualData.getCountry());
+        assertEquals(expectedData.getPhone(), actualData.getPhone());
+        assertEquals(expectedData.getEmail(), actualData.getEmail());
+        assertEquals(expectedData.getStreet1(), actualData.getStreet1());
+        assertEquals(expectedData.getStreet2(), actualData.getStreet2());
+        assertEquals(expectedData.getPostalCode(), actualData.getPostalCode());
+        assertEquals(expectedData.getStateProvince(), actualData.getStateProvince());
+
+    }
+
+
+
+    //delete contact
+
+    @Given("set the url for delete contact")
+    public void setTheUrlForDeleteContact() {
+        spec.pathParams("first", "contacts","second","66366fdc2e101a0013feadbe");
+
+
+    }
+
+    @When("send the delete request contact")
+    public void sendTheDeleteRequestContact() {
+        response = given(spec).delete("{first}/{second}");
+        response.prettyPrint();
+
+    }
+
+    @Then("do assertion for delete contact")
+    public void doAssertionForDeleteContact() {
+        assertEquals(200, response.statusCode());
+
+    }
 }
---
+++ src/test/resources/features/contact_list_contact/01_delete_contact.feature
@@ -0,0 +1,7 @@
+@Contact
+Feature: Delete Contact
+
+  Scenario: User can delete the Contact via API
+    Given set the url for delete contact
+    When send the delete request contact
+    Then do assertion for delete contact
